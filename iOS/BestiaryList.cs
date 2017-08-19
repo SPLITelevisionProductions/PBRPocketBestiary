1cb325aa75ae1255c044c814ad72242ad1aa147c
greenwarriorproductions <loganeaburrell@gmail.com>
2017-08-20 11:31:38 +1200

    Upgrade to WIndows VS2017

---
+++ iOS/BestiaryList.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace PBRBestiary.iOS
+{
+    public partial class BestiaryList : UITableViewController
+    {
+        public BestiaryList (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
